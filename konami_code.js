6d254ad5e843652c92066b2087932358df395c33
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-07 14:25:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,17 +11,17 @@ const codes = [
   "a"
 ];
 
-function sequenceChecker() {
-    const keyPressed = e.key;
-
-    if (keyPressed === codes[index]) {
-      index++
-
-      if (index === codes.length) {
-        alert("Nice one!");
-      }
-    }
-  }
+// function sequenceChecker() {
+//     const keyPressed = e.key;
+// 
+//     if (keyPressed === codes[index]) {
+//       index++
+// 
+//       if (index === codes.length) {
+//         alert("Nice one!");
+//       }
+//     }
+//   }
 
   function init() {
     let index = 0
